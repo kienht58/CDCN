df305a82ed47cc43ef59fd90c578673e3b0c7fb0
kienht58 <kien.isp15@gmail.com>
2016-06-09 20:15:52 +0700

    dashboard added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,11 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 use App\Models\Category;
+use App\Models\Game;
 
 class DashboardController extends Controller
 {
     public function index() {
     	$categories = Category::all();
-    	return view('welcome', compact('categories'));
+    	$popular_games = Game::orderBy('views', 'desc')->take(3)->get();
+    	$most_downloaded_game = Game::where('downloads', '>=', '1')->first();
+    	$recent_games = Game::orderBy('releaseTime', 'desc')->first();
+    	return view('welcome', compact('categories', 'popular_games', 'most_downloaded_games', 'recent_games'));
     }
 }
